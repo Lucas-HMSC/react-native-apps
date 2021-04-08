fc72e7eeafea5a8fafdf402c5b4f9b3f32819c51
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-04-08 19:40:30 -0300

    Aula 08 - Elementos Responsivos

--- app1/App.js
+++ app1/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import C1 from './componentes/comp1';
+import Caixas from './componentes/Caixas';
 import style from './estilos/Estilos';
 
 /* Formato Function */
 export default function App1() {
   return (
     <View style={style.container}>
-      <C1 curso="React Native" nota="10" />
-      <C1 curso="C++" nota="10" />
-      <C1 curso="Javascript" nota="10" />
-      <C1 curso="C#" nota="10" />
+      <Caixas />
       <Text style={style.textoPadrão}>youtube.com/cfbcursos</Text>
       <Text style={style.textoTitulo}>cfbcursos.com.br</Text>
     </View>
---
+++ app1/componentes/Caixas.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function (props) {
+  return (
+    <View
+      style={{
+        width: '100%',
+        height: 300,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}
+    >
+      <View
+        style={{
+          width: 50,
+          height: 50,
+          backgroundColor: '#2196F3',
+        }}
+      ></View>
+      <View
+        style={{
+          width: 50,
+          height: 50,
+          backgroundColor: '#3F51B5',
+        }}
+      ></View>
+      <View
+        style={{
+          width: 50,
+          height: 50,
+          backgroundColor: '#005',
+        }}
+      ></View>
+    </View>
+  );
+}
