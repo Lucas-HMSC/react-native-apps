50a2611ce70e69cda77c3a71dd1d3d46c0ed7d5d
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-04-07 21:00:36 -0300

    Create app1

---
+++ app1/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App1(){
+  return (
+    <View>
+      <View>
+        <Text>CFB Cursos</Text>
+        <Text>Curso de React Native - </Text>
+      </View>
+      <View>
+        <Text>Aula 3</Text>
+        <Text>www.cfbcursos.com.br</Text>
+      </View>
+    </View>
+  )
+}
