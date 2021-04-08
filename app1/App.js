e6795502002daeb8e9d35d3a2c09b22f38cc3d87
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-04-08 20:24:47 -0300

    Aula 11 - Inserindo Imagens

--- app1/App.js
+++ app1/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
 import Caixas from './componentes/Caixas';
 import style from './estilos/Estilos';
 
@@ -8,6 +8,11 @@ export default function App1() {
   let vexibir = true;
   return (
     <View style={style.container}>
+      <Image 
+        // source={require('./assets/icon.png')} 
+        source={{uri:'https://cfbcursos.com.br/logoCanal.png'}} 
+        style={estilo.logo}
+      />
       <Caixas exibir={vexibir} />
       <Text style={style.textoPadrão}>youtube.com/cfbcursos</Text>
       <Text style={style.textoTitulo}>cfbcursos.com.br</Text>
@@ -16,3 +21,11 @@ export default function App1() {
     </View>
   );
 }
+
+const estilo = StyleSheet.create({
+  logo: {
+    width: '100%',
+    height: 200,
+    resizeMode: 'contain'
+  },
+});
