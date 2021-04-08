126c2277b8b641d47e874b50a842a18567ce5469
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-04-07 21:41:55 -0300

    Aula 05 - Formatação CSS nos elementos

--- app1/App.js
+++ app1/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import C1 from './componentes/comp1';
 
 /* Formato Function */
 export default function App1() {
   return (
-    <View>
+    <View style={estilos.container}>
       <C1 />
-      <Text>youtube.com/cfbcursos</Text>
+      <Text style={estilos.txt0}>youtube.com/cfbcursos</Text>
+      <Text style={{ color: '#f50', fontSize: 30 }}>cfbcursos.com.br</Text>
     </View>
   );
 }
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#444',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  txt0: {
+    color: '#080',
+    fontSize: 10,
+  },
+});
 
 /* Formato Class
 export default class App1 extends Component {
--- app1/componentes/comp1.js
+++ app1/componentes/comp1.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import {Text} from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 
-export default function(){
-  return (
-    <Text>
-      CFB Cursos - Curso de React Native
-    </Text>
-  )
+export default function () {
+  return <Text style={estilos.txt2}>CFB Cursos - Curso de React Native</Text>;
 }
+
+const estilos = StyleSheet.create({
+  txt1: {
+    color: '#f00',
+    fontSize: 20,
+  },
+  txt2: {
+    color: '#00f',
+    fontSize: 15,
+  },
+});
