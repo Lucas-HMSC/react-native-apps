3a07210e86f11898e0db7c52055caf2ff06c376d
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-04-08 19:20:50 -0300

    Aula 07 - CSS Externo

--- app1/componentes/comp1.js
+++ app1/componentes/comp1.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { Text } from 'react-native';
+import style from '../estilos/Estilos';
 
-let nt, cor;
+let nt;
 
 export default function (props) {
   nt = props.nota;
-  cor = props.cor;
   return (
-    <Text style={estilos.txt2, {color: cor}}>
+    <Text style={style.textoCursos}>
       CFB Cursos - Curso de {props.curso}: Nota = {nt}
     </Text>
   );
 }
-
-const estilos = StyleSheet.create({
-  txt1: {
-    color: '#f00',
-    fontSize: 20,
-  },
-  txt2: {
-    color: '#00f',
-    fontSize: 15,
-  },
-});
