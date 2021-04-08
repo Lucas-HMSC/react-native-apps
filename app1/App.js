1af52be058e2c8b07307fd719dd80046309e31d6
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-04-07 21:22:59 -0300

    Aula 04 - Criando o primeiro component

--- app1/App.js
+++ app1/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import C1 from './componentes/comp1';
 
-export default function App1(){
+/* Formato Function */
+export default function App1() {
   return (
     <View>
-      <View>
-        <Text>CFB Cursos</Text>
-        <Text>Curso de React Native - </Text>
-      </View>
-      <View>
-        <Text>Aula 3</Text>
-        <Text>www.cfbcursos.com.br</Text>
-      </View>
+      <C1 />
+      <Text>youtube.com/cfbcursos</Text>
     </View>
-  )
+  );
 }
+
+/* Formato Class
+export default class App1 extends Component {
+  render(){
+    return (
+      <View>
+        <View>
+          <Text>CFB Cursos</Text>
+          <Text>Curso de React-Native</Text>
+        </View>
+        <View>
+          <Text>Aula 3</Text>
+          <Text>www.cfbcursos.com.br</Text>
+        </View>
+      </View>
+    )
+  }
+}
+*/
