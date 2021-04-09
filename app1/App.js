765e39a73d032e2c3e35af5faca91d4b7b55888b
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-04-08 21:51:07 -0300

    Aula 14 - FlatList

--- app1/App.js
+++ app1/App.js
@@ -1,34 +1,15 @@
 import React from 'react';
-import { Text, View, Image, StyleSheet, Button, Alert } from 'react-native';
-import Caixas from './componentes/Caixas';
+import { Text, View, Image, StyleSheet } from 'react-native';
+import Lp from './componentes/ListaPlana';
 import style from './estilos/Estilos';
 
-/* Formato Function */
 export default function App1() {
   let vexibir = false;
   return (
     <View style={style.container}>
-      <Button 
-        title='Mostrar Mensagem'
-        onPress={() => Alert.alert('Aviso', 'Curso de React Native')}
-      />
-      <Image
-        // source={require('./assets/icon.png')}
-        source={{ uri: 'https://cfbcursos.com.br/logoCanal.png' }}
-        style={estilo.logo}
-      />
-      <Text style={style.textoPadrão}>youtube.com/cfbcursos</Text>
-      <Text style={style.textoTitulo}>cfbcursos.com.br</Text>
-      {vexibir ? <Text>Curso de React Native</Text> : <Text>- - -</Text>}
-      {vexibir && <Text>Renderizado com Operador AND</Text>}
+      <Text>CFB Cursos</Text>
+      <Text>Curso de React Native</Text>
+      <Lp />
     </View>
   );
 }
-
-const estilo = StyleSheet.create({
-  logo: {
-    width: '100%',
-    height: 200,
-    resizeMode: 'contain',
-  },
-});
