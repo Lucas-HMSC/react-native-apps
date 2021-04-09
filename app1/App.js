832cdf70b311580a893444b2cb0a538eb3388c49
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-04-09 18:24:46 -0300

    Aula 15 - Imagem de Fundo

--- app1/App.js
+++ app1/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import { Text, View, Image, StyleSheet } from 'react-native';
+import { Text, View, Image, StyleSheet, ImageBackground } from 'react-native';
 import Lp from './componentes/ListaPlana';
 import style from './estilos/Estilos';
+
+const image = { uri: 'https://reactjs.org/logo-og.png' };
 
 export default function App1() {
   let vexibir = false;
   return (
     <View style={style.container}>
-      <Text>CFB Cursos</Text>
-      <Text>Curso de React Native</Text>
-      <Lp />
+      <ImageBackground source={image} style={estilos.imagemFundo}>
+        <View style={estilos.componentes}>
+          <Text style={estilos.texto}>CFB Cursos</Text>
+          <Text style={estilos.texto}>Curso de React Native</Text>
+          {/* <Lp /> */}
+        </View>
+      </ImageBackground>
     </View>
   );
 }
+
+const estilos = StyleSheet.create({
+  imagemFundo: {
+    flex: 1,
+    resizeMode: 'cover',
+    width: '100%',
+  },
+  componentes: {
+    flex: 1,
+    padding: 10,
+    alignSelf: 'center',
+  },
+  texto: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+});
