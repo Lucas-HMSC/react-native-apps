a74e9f6e22b1d89bdc2f5c1c70bfc99832e6af0c
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-04-10 12:15:38 -0300

    Aula 19 - TextInput

--- app1/App.js
+++ app1/App.js
@@ -1,32 +1,14 @@
 import React, { useState } from 'react';
 import { Text, StyleSheet, SafeAreaView } from 'react-native';
 import style from './estilos/Estilos';
-import CarroF from './componentes/CarroF';
+import CxTx from './componentes/CaixaDeTexto';
 
 export default function App1() {
   return (
     <SafeAreaView style={style.container}>
       <Text>CFB Cursos</Text>
       <Text>Curso de React Native</Text>
-      <CarroF nome="Polo" />
-      <CarroF nome="Fit" />
+      <CxTx />
     </SafeAreaView>
   );
 }
-
-const estilos = StyleSheet.create({
-  imagemFundo: {
-    flex: 1,
-    resizeMode: 'cover',
-    width: '100%',
-  },
-  componentes: {
-    flex: 1,
-    padding: 10,
-    alignSelf: 'center',
-  },
-  texto: {
-    color: '#fff',
-    textAlign: 'center',
-  },
-});
