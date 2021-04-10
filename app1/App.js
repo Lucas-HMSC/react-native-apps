d67f96a5f595c51f5c13627aa70da2f5e6ced9ef
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-04-10 16:02:08 -0300

    Aula 20 - ScrollView

--- app1/App.js
+++ app1/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { Text, StyleSheet, SafeAreaView } from 'react-native';
 import style from './estilos/Estilos';
-import CxTx from './componentes/CaixaDeTexto';
+import Rolagem from './componentes/Rolagem';
 
 export default function App1() {
   return (
     <SafeAreaView style={style.container}>
       <Text>CFB Cursos</Text>
       <Text>Curso de React Native</Text>
-      <CxTx />
+      <Rolagem />
     </SafeAreaView>
   );
 }
---
+++ app1/componentes/Rolagem.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { View, Text, ScrollView } from 'react-native';
+
+export default function () {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <ScrollView
+        style={{
+          backgroundColor: '#333',
+          padding: 15,
+        }}
+      >
+        <Text
+          style={{
+            color: '#eee'
+          }}
+        >
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam
+          cumque in ut molestiae consequatur delectus tenetur voluptatibus
+          minus, autem laudantium, ducimus, soluta nemo repellendus repudiandae
+          sint. Sint eius ipsam itaque!Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. Veniam cumque in ut molestiae consequatur delectus
+          tenetur voluptatibus minus, autem laudantium, ducimus, soluta nemo
+          repellendus repudiandae sint. Sint eius ipsam itaque!Lorem ipsum dolor
+          sit, amet consectetur adipisicing elit. Veniam cumque in ut molestiae
+          consequatur delectus tenetur voluptatibus minus, autem laudantium,
+          ducimus, soluta nemo repellendus repudiandae sint. Sint eius ipsam
+          itaque!Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Veniam cumque in ut molestiae consequatur delectus tenetur
+          voluptatibus minus, autem laudantium, ducimus, soluta nemo repellendus
+          repudiandae sint. Sint eius ipsam itaque!Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Veniam cumque in ut molestiae
+          consequatur delectus tenetur voluptatibus minus, autem laudantium,
+          ducimus, soluta nemo repellendus repudiandae sint. Sint eius ipsam
+          itaque!Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Veniam cumque in ut molestiae consequatur delectus tenetur
+          voluptatibus minus, autem laudantium, ducimus, soluta nemo repellendus
+          repudiandae sint. Sint eius ipsam itaque!Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Veniam cumque in ut molestiae
+          consequatur delectus tenetur voluptatibus minus, autem laudantium,
+          ducimus, soluta nemo repellendus repudiandae sint. Sint eius ipsam
+          itaque!Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Veniam cumque in ut molestiae consequatur delectus tenetur
+          voluptatibus minus, autem laudantium, ducimus, soluta nemo repellendus
+          repudiandae sint. Sint eius ipsam itaque!Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Veniam cumque in ut molestiae
+          consequatur delectus tenetur voluptatibus minus, autem laudantium,
+          ducimus, soluta nemo repellendus repudiandae sint. Sint eius ipsam
+          itaque!Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Veniam cumque in ut molestiae consequatur delectus tenetur
+          voluptatibus minus, autem laudantium, ducimus, soluta nemo repellendus
+          repudiandae sint. Sint eius ipsam itaque!Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Veniam cumque in ut molestiae
+          consequatur delectus tenetur voluptatibus minus, autem laudantium,
+          ducimus, soluta nemo repellendus repudiandae sint. Sint eius ipsam
+          itaque!
+        </Text>
+      </ScrollView>
+    </View>
+  );
+}
