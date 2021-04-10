3e651e9bc5ce37c1a4c3ef1e2bf71504eead4417
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-04-10 11:14:47 -0300

    Aula 18 - Componente Switch

--- app1/App.js
+++ app1/App.js
@@ -1,19 +1,16 @@
 import React, { useState } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, StyleSheet, SafeAreaView } from 'react-native';
 import style from './estilos/Estilos';
-import CarroC from './componentes/CarroC';
 import CarroF from './componentes/CarroF';
 
 export default function App1() {
   return (
-    <View style={style.container}>
+    <SafeAreaView style={style.container}>
       <Text>CFB Cursos</Text>
       <Text>Curso de React Native</Text>
-      <CarroC nome="Golf" />
-      <CarroC nome="HRV" />
       <CarroF nome="Polo" />
       <CarroF nome="Fit" />
-    </View>
+    </SafeAreaView>
   );
 }
 
--- app1/componentes/CarroF.js
+++ app1/componentes/CarroF.js
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Switch } from 'react-native';
 
 export default function (props) {
   const [ligado, setLigado] = useState(true);
+  // const toggleLigado = () => setLigado(!ligado);
+  // Outra forma:
+  const toggleLigado = () => setLigado((previousState) => !previousState);
   return (
     <View>
       <Text>
         Carro: {props.nome} - Ligado: {ligado ? 'Sim' : 'Não'}
       </Text>
-      <Button
-        title={ligado ? 'Desligar' : 'Ligar'}
-        onPress={() => setLigado(!ligado)}
+      <Switch
+        trackColor={{ false: '#777', true: '#8bf' }}
+        thumbColor={ligado ? '#00f' : '#444'}
+        value={ligado}
+        onValueChange={toggleLigado}
       />
     </View>
   );
