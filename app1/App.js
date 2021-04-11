76588a7671b461050705b612975c60b995c1f4ff
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-04-11 10:18:44 -0300

    Aula 24 - StatusBar

--- app1/App.js
+++ app1/App.js
@@ -1,14 +1,43 @@
 import React, { useState } from 'react';
-import { Text, StyleSheet, SafeAreaView } from 'react-native';
+import {
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  Button,
+} from 'react-native';
 import style from './estilos/Estilos';
-import Rolagem from './componentes/Rolagem';
 
 export default function App1() {
+  const [cor, setCor] = useState('#222');
+
   return (
     <SafeAreaView style={style.container}>
+      <StatusBar
+        backgroundColor={cor}
+        barStyle="light-content"
+        hidden={false}
+      />
       <Text>CFB Cursos</Text>
       <Text>Curso de React Native</Text>
-      <Rolagem />
+      <Button
+        title="Vermelho"
+        onPress={() => {
+          setCor('#D32F2F');
+        }}
+      />
+      <Button
+        title="Azul"
+        onPress={() => {
+          setCor('#303F9F');
+        }}
+      />
+      <Button
+        title="Verde"
+        onPress={() => {
+          setCor('#2E7D32');
+        }}
+      />
     </SafeAreaView>
   );
 }
