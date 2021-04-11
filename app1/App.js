f51e84d6c33a14771cc61d251d2ba3ca8db72e28
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-04-10 22:17:12 -0300

    Aula 23 - RefreshControl

--- app1/App.js
+++ app1/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { Text, StyleSheet, SafeAreaView } from 'react-native';
 import style from './estilos/Estilos';
-import Modal from './componentes/Modal';
+import Rolagem from './componentes/Rolagem';
 
 export default function App1() {
   return (
     <SafeAreaView style={style.container}>
       <Text>CFB Cursos</Text>
       <Text>Curso de React Native</Text>
-      <Modal />
+      <Rolagem />
     </SafeAreaView>
   );
 }
--- app1/componentes/Rolagem.js
+++ app1/componentes/Rolagem.js
@@ -1,7 +1,16 @@
-import React from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, ScrollView, RefreshControl } from 'react-native';
 
 export default function () {
+  const [atualizando, setAtualizando] = useState(false);
+
+  const aoAtualizar = () => {
+    setAtualizando(true);
+    setTimeout(() => {
+      setAtualizando(false);
+    }, 1000);
+  };
+
   return (
     <View
       style={{
@@ -13,10 +22,13 @@ export default function () {
           backgroundColor: '#333',
           padding: 15,
         }}
+        refreshControl={
+          <RefreshControl refreshing={atualizando} onRefresh={aoAtualizar} />
+        }
       >
         <Text
           style={{
-            color: '#eee'
+            color: '#eee',
           }}
         >
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam
@@ -57,7 +69,10 @@ export default function () {
           consectetur adipisicing elit. Veniam cumque in ut molestiae
           consequatur delectus tenetur voluptatibus minus, autem laudantium,
           ducimus, soluta nemo repellendus repudiandae sint. Sint eius ipsam
-          itaque!
+          itaque! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste
+          dignissimos assumenda hic, vitae delectus explicabo quia optio! Error
+          dicta, cupiditate natus accusantium quos hic corporis quibusdam,
+          consequatur eveniet voluptatem deleniti!
         </Text>
       </ScrollView>
     </View>
