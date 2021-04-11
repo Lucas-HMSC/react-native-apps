1dac4ba15faa4c2c52471d61289d316f7ceb0025
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-04-11 18:51:10 -0300

    Aula 30 - Variaveis Globais com Static

--- app1/App.js
+++ app1/App.js
@@ -1,98 +1,19 @@
-import React from 'react';
-import { Text, StyleSheet, View, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Globais from './componentes/Globais';
 
-const Gavetas = createDrawerNavigator();
+export default class App1 extends Component {
+  nome = Globais.nome;
+  canal = Globais.canal;
+  num = Globais.num;
 
-function TelaHome({ navigation }) {
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <Text>Tela Home</Text>
-      <Text>CFB Cursos</Text>
-    </View>
-  );
-}
-
-function TelaCanal({ navigation }) {
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <Text>Tela Canal</Text>
-      <Text>youtube.com/cfbcursos</Text>
-    </View>
-  );
-}
-
-function TelaCursos({ navigation }) {
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <Text>Tela Cursos</Text>
-    </View>
-  );
-}
-
-export default function App1() {
-  return (
-    <NavigationContainer>
-      <Gavetas.Navigator initialRouteName="Home">
-        <Gavetas.Screen
-          name="Home"
-          component={TelaHome}
-          options={{
-            title: 'Tela Inicial',
-            headerStyle: {
-              backgroundColor: '#4FC3F7',
-            },
-            headerTintColor: '#E1F5FE',
-            headerRight: () => (
-              <Button
-                title="Cursos"
-                color="#222"
-                onPress={() => alert('Botão Cursos Clicado')}
-              />
-            ),
-          }}
-        />
-        <Gavetas.Screen
-          name="Canal"
-          component={TelaCanal}
-          options={{
-            title: 'Tela Canal',
-            headerStyle: {
-              backgroundColor: '#4FC3F7',
-            },
-            headerTintColor: '#E1F5FE',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-          }}
-        />
-        <Gavetas.Screen
-          name="Cursos"
-          component={TelaCursos}
-          options={{
-            title: 'Cursos do Canal',
-          }}
-        />
-      </Gavetas.Navigator>
-    </NavigationContainer>
-  );
+  render() {
+    return (
+      <View>
+        <Text>Nome: {this.nome}</Text>
+        <Text>Canal: {this.canal}</Text>
+        <Text>Número: {this.num}</Text>
+      </View>
+    );
+  }
 }
---
+++ app1/componentes/Globais.js
@@ -0,0 +1,7 @@
+class Globais {
+  static nome = 'Bruno';
+  static canal = 'CFB Cursos';
+  static num = 2020;
+}
+
+export default Globais;
